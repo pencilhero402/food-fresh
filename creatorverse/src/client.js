f63f37d7cb75a0a04d5f05ddaf9e8f58813e51cf
pencilhero402 <axinl2703@gmail.com>
2025-08-29 12:09:38 -0400

    Delete creatorverse directory

--- creatorverse/src/client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://auowdbtlxioogpnzjkcl.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1b3dkYnRseGlvb2dwbnpqa2NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyOTMwMTIsImV4cCI6MjA3MDg2OTAxMn0.d5Z0F9bNC1Rk9I_gBVOm4oxYKOtq1vh2uCm8bg-xxMQ';
-export const supabase = createClient(URL, API_KEY);
